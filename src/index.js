84ce9d273930401b388143667d92b608c0994b9c
Ryan Palo <ryan@thepalos.com>
2018-02-27 14:40:48 -0800

    I forgot to use git, so here's the whole project in one commit...

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import loadContent from './loadContent.js';
+
+loadContent();
+
---
+++ src/loadNav.js
@@ -0,0 +1,16 @@
+import openTab from './openTab';
+
+const loadNav = () => {
+  const tabHolder = document.querySelector('.tabs');
+  const tabs = ['About', 'Menu', 'Contact'];
+  tabs.forEach(tabName => {
+  const button = document.createElement('button');
+  button.classList.add('tabs__link');
+  button.addEventListener('click', openTab(tabName));
+  button.innerHTML = tabName;
+  tabHolder.appendChild(button);
+  });
+};
+
+export default loadNav;
+
