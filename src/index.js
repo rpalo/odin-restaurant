b8db6a4edb2f3c10beea850cea12c61f643b4471
Ryan Palo <ryan@thepalos.com>
2018-02-27 15:00:40 -0800

    Improve how it looks and works on load

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import loadContent from './loadContent.js';
 
 loadContent();
 
+const about = document.querySelector('[data-page="About"]');
+about.style.display = "block";
+const aboutButton = document.querySelector('[data-target="About"]');
+aboutButton.classList.add('active');
--- src/loadNav.js
+++ src/loadNav.js
@@ -6,6 +6,7 @@ const loadNav = () => {
   tabs.forEach(tabName => {
   const button = document.createElement('button');
   button.classList.add('tabs__link');
+  button.dataset.target = tabName;
   button.addEventListener('click', openTab(tabName));
   button.innerHTML = tabName;
   tabHolder.appendChild(button);
